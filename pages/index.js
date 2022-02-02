c716e3dc6b163c3fd5febbb39e30bf9d9cf0efc4
Nick Land <land.d.nicholas@gmail.com>
2022-02-01 20:16:46 -0500

    ✨ Finish initial landing page
    * Remove cypress

--- pages/index.js
+++ pages/index.js
@@ -21,18 +21,24 @@ export default function Home() {
     );
   }
   return (
-    <div className="flex flex-col justify-center w-full h-screen">
-      <div className="p-4 space-y-4 self-center text-center">
-        <span className="text-xl font-extrabold">
-          Not signed in <br />
-        </span>
-        <SocialButton
-          textColor="text-white"
-          buttonText="Sign in with Github!"
-          buttonColor="bg-neutral-800"
-          Icon={<FontAwesomeIcon icon={faGithub} alt="Github Icon" inverse />}
-          onClick={() => signIn('github')}
-        />
+    <div className="flex flex-col h-screen">
+      <h3 className="flex-none text-center text-lg sm:text-3xl p-4 font-bold text-slate-700">
+        Welcome to the movie &amp; TV show tracker!
+      </h3>
+      <div className="grow flex flex-col justify-center w-full">
+        <div className="p-4 space-y-4 self-center text-center">
+          <span className="text-xl font-extrabold">
+            Please sign in! <br />
+          </span>
+          <SocialButton
+            className="login"
+            textColor="text-white"
+            buttonText="Sign in with Github!"
+            buttonColor="bg-neutral-800"
+            Icon={<FontAwesomeIcon icon={faGithub} alt="Github Icon" inverse />}
+            onClick={() => signIn('github')}
+          />
+        </div>
       </div>
     </div>
   );
