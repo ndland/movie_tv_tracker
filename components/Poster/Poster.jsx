fb1bddc3ed61d5c04ea4f5ff4dc73e3985df336d
Nick Land <land.d.nicholas@gmail.com>
2022-03-24 20:43:48 -0400

    💄Add rounded corners to Poster image

--- components/Poster/Poster.jsx
+++ components/Poster/Poster.jsx
@@ -5,6 +5,7 @@ const Poster = ({ info: { results } }) => {
     <div className="relative w-48 h-64">
       <Image
         alt="image"
+        className="rounded-md"
         aria-label={`${results[0].original_title}`}
         src={`https://image.tmdb.org/t/p/original${results[0].poster_path}`}
         objectFit="cover"
